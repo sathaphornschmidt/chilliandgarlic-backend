4da6ec81795c0674a6b8de5c74aff54cc422f74d
Sathaphorn Laphrom <sathaphorn.schmidt@gmail.com>
2026-03-24 16:24:02 +0100

    Fix knex production config

--- knexfile.ts
+++ knexfile.ts
@@ -3,24 +3,29 @@ import { config as dotenvConfig } from "dotenv";
 
 dotenvConfig();
 
-const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: "postgresql",
-    connection: {
-      server: process.env.DB_HOST!,
-      database: process.env.DB_NAME!,
-      user: process.env.DB_USER!,
-      password: process.env.DB_PASSWORD!,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: './src/databases/migrations',
-    },
+const sharedConfig: Knex.Config = {
+  client: "postgresql",
+  connection: {
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    database: process.env.DB_NAME!,
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    ssl: { rejectUnauthorized: false },
   },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./src/databases/migrations",
+  },
+};
+
+const config: { [key: string]: Knex.Config } = {
+  development: sharedConfig,
+  production: sharedConfig,
 };
 
 export default config;
