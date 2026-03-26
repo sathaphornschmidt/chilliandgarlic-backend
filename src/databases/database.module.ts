f55d1841b4d7a35d0a9d32084e78feb2556e8503
Sathaphorn Laphrom <sathaphorn.schmidt@gmail.com>
2026-03-26 09:40:41 +0100

    fix database module imports

--- src/databases/database.module.ts
+++ src/databases/database.module.ts
@@ -1,37 +1,36 @@
-// database.module.ts
 import { Global, Module, Provider } from '@nestjs/common';
 import knex from 'knex';
-import knexConfig from '../../knexfile'; // Import your knexfile configuration
-import { unitOfWorkFactory } from './unit-of-work/unitOfWorkFactory';
+const knexConfig = require('../../knexfile');
+import { UnitOfWorkFactory } from './unit-of-work/unitOfWorkFactory';
 
-const environment = process.env.NODE_ENV || 'development'; // Default to 'development'
+const environment = process.env.NODE_ENV || 'development';
 const knexInstance = knex(knexConfig[environment]);
 
 const knexProvider: Provider = {
-  provide: 'KNEX_INSTANCE', // Token to inject Knex instance
+  provide: 'KNEX_INSTANCE',
   useValue: knexInstance,
 };
 
 const databaseHealthCheckProvider: Provider = {
-    provide: 'DATABASE_HEALTH_CHECK',
-    useFactory: async () => {
-      try {
-        await knexInstance.raw('SELECT 1+1 AS result');
-        console.log('Database connection is healthy');
-      } catch (error) {
-        console.error('Database connection failed', error);
-        throw error;
-      }
-    },
-  }
+  provide: 'DATABASE_HEALTH_CHECK',
+  useFactory: async () => {
+    try {
+      await knexInstance.raw('SELECT 1+1 AS result');
+      console.log('Database connection is healthy');
+    } catch (error) {
+      console.error('Database connection failed', error);
+      throw error;
+    }
+  },
+};
 
 @Global()
 @Module({
   providers: [
     knexProvider,
     databaseHealthCheckProvider,
-    unitOfWorkFactory,
-  ], // Register Knex instance and unitOfWorkFactory
-  exports: [knexProvider, unitOfWorkFactory], // Export them for use in other modules
+    UnitOfWorkFactory,
+  ],
+  exports: [knexProvider, UnitOfWorkFactory],
 })
 export class DatabaseModule {}
